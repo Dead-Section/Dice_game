e314b2f4797b67ee2ec9ac28922315850520e604
hussein bahdon <35459369+HusseinMccoin@users.noreply.github.com>
2022-11-21 20:16:18 +0000

    add the function to change the dice

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
+//this function reload the page
+function reload() {
+  var buttonReload = document.querySelector("button");
+  buttonReload = window.location.reload();
+}
 
-  var arrayImage = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]
+//<---------------------------------------------------------------->
 
-function randomSelction() {
-  var x = document.querySelector("img").setAttribute(item);
+var randonNumber, randonNumber2;
 
-  var n = Math.random();
-  var item = arrayImage[n];
+randonNumber = Math.floor(Math.random() * 6) + 1
+randonNumber2 = Math.floor(Math.random() * 6) + 1
+
+var imgLayout = "images/dice" + randonNumber + ".png";
+var imgLayout2 = "images/dice" + randonNumber2 + ".png";
+console.log(randonNumber, randonNumber2)
+
+function changeImg1() {
+  var img1 = document.querySelectorAll("img");
+  //Player1
+  img1[0].setAttribute("src", imgLayout);
+  //Player2
+  img1[1].setAttribute("src", imgLayout2);
 }
 
 
-function reload(){
-  var pageReload = document.querySelector("button");
-  pageReload = window.location.reload();
-}
+//<---------------------------------------------------------------->
 
 
-
-
-  // <button onClick="window.location.reload();">Refresh Page</button>
+changeImg1();
